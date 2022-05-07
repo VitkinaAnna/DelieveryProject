327d288c46b03661c821c1bbf6b17c0afffbd2b6
VitkinaAnna <94934830+VitkinaAnna@users.noreply.github.com>
2022-05-07 20:12:13 +0300

    Add files via upload

---
+++ Models/Menu.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace DelieveryWebApplication
+{
+    public partial class Menu
+    {
+        public Menu()
+        {
+            Delieveries = new HashSet<Delievery>();
+        }
+
+        public int DishId { get; set; }
+
+        [Required(ErrorMessage = "Поле не повинне бути порожнім")]
+        [Display(Name = "Назва страви")]
+        public string Name { get; set; } = null!;
+
+        [Required(ErrorMessage = "Поле не повинне бути порожнім")]
+        [Display(Name = "Ціна")]
+        public int Price { get; set; }
+        public int TypeId { get; set; }
+
+        [Display(Name = "Тип страви")]
+        public virtual DishType Type { get; set; } = null!;
+        public virtual ICollection<Delievery> Delieveries { get; set; }
+    }
+}
---
+++ Models/Order.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace DelieveryWebApplication
+{
+    public partial class Order
+    {
+        public Order()
+        {
+            Delieveries = new HashSet<Delievery>();
+        }
+
+        public int OrderId { get; set; }
+        public int ClientId { get; set; }
+        public int CourierId { get; set; }
+
+        [Required(ErrorMessage = "Поле не повинне бути порожнім")]
+        [Display(Name = "Час замовлення")]
+        public DateTime Time { get; set; }
+
+        [Required(ErrorMessage = "Поле не повинне бути порожнім")]
+        [Display(Name = "Час доставки")]
+        public DateTime DelieveryTime { get; set; }
+
+        [Display(Name = "Клієнт")]
+        public virtual Client Client { get; set; } = null!;
+        [Display(Name = "Кур`єр")]
+        public virtual Courier Courier { get; set; } = null!;
+        public virtual ICollection<Delievery> Delieveries { get; set; }
+    }
+}
